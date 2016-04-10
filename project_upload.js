b65033e96762508a23755f641193f5118a3c70da
chirags <chiragshah271093@gmail.com>
2016-04-10 18:55:19 -0400

    Adding basic server

---
+++ project_upload.js
@@ -0,0 +1,39 @@
+var AWS = require('aws-sdk'); 
+
+var s3 = new AWS.S3(); 
+
+AWS.config.update(
+  {
+    accessKeyId: "AKIAIRZG7PNLRNL3K4MA",
+    secretAccessKey: "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",
+  }
+);
+
+
+//  s3.createBucket({Bucket: 'databucketcis550'}, function() {
+
+//   var params = {Bucket: 'databucketcis550', Key: 'myKey', Body: 'Hello!'};
+
+//   s3.putObject(params, function(err, data) {
+
+//       if (err)       
+
+//           console.log(err)     
+
+//       else       console.log("Successfully uploaded data to myBucket/myKey");   
+
+//    });
+
+// });
+
+s3.getObject(
+  { Bucket: "databucketcis550", Key: "Resume_Chirag_M_Shah.pdf" },
+  function (err, data) {
+    if (err != null) {
+      console.log(err);
+    } else {
+      console.log(data.Body.toString('ascii'));
+      // do something with data.body
+    }
+  }
+);
